c1c766861102f6428e3c802cfb1b6b7591886168
mehrshadFarzaneh <50589065+mehrshadFarzaneh@users.noreply.github.com>
2022-09-01 09:59:37 +0430

    Initial Project and Git -- Squash --
    initial commit
    
    Initial Git
    
    initial git

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Step_By_Step_For_Angular_Essentials';
+}
